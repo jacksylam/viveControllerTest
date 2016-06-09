e19159139726073317c2b9ab2d4db0c1a4ee7b4a
Jack <catinacan@gmail.com>
2016-06-08 21:08:37 -1000

    Initial commit.

---
+++ viveControllerTest/Assets/ControlTest.cs
@@ -0,0 +1,38 @@
+﻿using UnityEngine;
+using System.Collections;
+
+[RequireComponent(typeof(SteamVR_TrackedObject))]
+public class ControlTest : MonoBehaviour {
+    public SteamVR_TrackedObject rightTrackedObj;
+    public SteamVR_TrackedObject leftTrackedObj;
+
+
+	// Use this for initialization
+	void Start () {
+	
+	}
+	
+    void Awake() {
+        rightTrackedObj = rightTrackedObj.GetComponent<SteamVR_TrackedObject>();
+        leftTrackedObj = leftTrackedObj.GetComponent<SteamVR_TrackedObject>();
+    }
+
+    void Update() {
+        SteamVR_Controller.Device rightDevice = SteamVR_Controller.Input((int)rightTrackedObj.index);
+        SteamVR_Controller.Device leftDevice = SteamVR_Controller.Input((int)leftTrackedObj.index);
+
+        if (rightDevice.GetTouch(SteamVR_Controller.ButtonMask.Trigger)) {
+            Debug.Log("You have pulled the RIGHT trigger");
+
+        }
+        if (rightDevice.GetTouch(SteamVR_Controller.ButtonMask.Grip)) {
+
+            Debug.Log("You have Pressed the RIGHT grip");
+        }
+
+
+    }
+}
+
+
+
